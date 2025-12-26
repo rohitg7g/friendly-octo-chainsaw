ce656be1abd12dbbdb415f3c9e2e45c28307d78f
rohitg7g <rohitchaudhary21909@gmail.com>
2025-12-26 16:32:53 +0530

    Update vars.py

--- Drm-bot-543-main/vars.py
+++ Drm-bot-543-main/vars.py
@@ -3,23 +3,23 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24712525"))
-API_HASH = environ.get("API_HASH", "6487082b6952f0d981f5f23d97c32992")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7656543932:AAHXMBSgKB58H3ITUfQJsvnet_DkOqzdA7c")
+API_ID = int(environ.get("API_ID", "37166634"))
+API_HASH = environ.get("API_HASH", "36ffee547af7f9eca0a24be19aa7ad30")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8433913568:AAFRPshrSIBhgmHKgFoXv4F7ZKjOtTWNzpc")
+OWNER = int(environ.get("OWNER", "5799395577"))
+CREDIT = environ.get("CREDIT", "💫『 @OHITGOAT 』💫")
 
-OWNER = int(environ.get("OWNER", "6334323103"))
-CREDIT = environ.get("CREDIT", "💫『 𝒟𝒾𝓋𝓎𝒶𝓃𝓈𝒽 𝓈𝒽𝓊𝓀𝓁𝒶 』💫")
-
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6334323103').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5799395577').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '6334323103').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5799395577').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
 
